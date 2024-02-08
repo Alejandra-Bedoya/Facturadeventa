a64f45b63892ca3f55676ff1cf7a503139012d2a
Alejandra Bedoya <cesde.alejandra@gmail.com>
2024-02-07 23:09:39 -0500

    Calculos matemáticos

--- src/Facturadeventa.py
+++ src/Facturadeventa.py
@@ -1,3 +1,9 @@
+def imprimir_factura(producto, cantidad, precio):
+    subtotal = cantidad * precio
+    descuento = subtotal * 0.25
+    subtotal_con_descuento = subtotal - descuento
+    iva = subtotal_con_descuento * 0.19
+
 producto = input('Ingrese el nombre del producto: ')
 cantidad = int(input('Ingrese la cantidad del producto: '))
 precio = float(input('Ingrese el precio del producto: $'))
